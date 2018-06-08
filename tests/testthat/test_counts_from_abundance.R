bd260471d4f8a423820968b5527c9718f07988a1
mikelove <michaelisaiahlove@gmail.com>
2018-06-08 11:39:02 -0400

    sumToGene with cfa=no after tximport txOut=TRUE and cfa!=no

--- tests/testthat/test_counts_from_abundance.R
+++ tests/testthat/test_counts_from_abundance.R
@@ -7,27 +7,41 @@ test_that("getting counts from abundance works", {
   files <- file.path(dir,"salmon", samples$run, "quant.sf.gz")
   names(files) <- paste0("sample",1:6)
   tx2gene <- read.csv(file.path(dir, "tx2gene.gencode.v27.csv"))
-  
-  txi.cfa <- tximport(files, type="salmon", tx2gene=tx2gene, 
+
+  txi <- tximport(files, type="salmon", tx2gene=tx2gene)
+  txi.S <- tximport(files, type="salmon", tx2gene=tx2gene, 
                       countsFromAbundance="scaledTPM")
-  txi.cfa.len <- tximport(files, type="salmon", tx2gene=tx2gene, 
+  txi.LS <- tximport(files, type="salmon", tx2gene=tx2gene, 
                           countsFromAbundance="lengthScaledTPM")
   
-  expect_true(ncol(txi.cfa$counts) == length(files))
-  expect_true(ncol(txi.cfa.len$counts) == length(files))
+  expect_true(ncol(txi.S$counts) == length(files))
+  expect_true(ncol(txi.LS$counts) == length(files))
 
   # also txOut=TRUE
 
-  txi <- tximport(files, type="salmon", txOut=TRUE,
-                  countsFromAbundance="no")
-  txi.cfa <- tximport(files, type="salmon", txOut=TRUE,
-                      countsFromAbundance="scaledTPM")
-  txi.cfa.len <- tximport(files, type="salmon", txOut=TRUE,
-                          countsFromAbundance="lengthScaledTPM")
+  txi.tx <- tximport(files, type="salmon", txOut=TRUE,
+                     countsFromAbundance="no")
+  txi.tx.S <- tximport(files, type="salmon", txOut=TRUE,
+                         countsFromAbundance="scaledTPM")
+  txi.tx.LS <- tximport(files, type="salmon", txOut=TRUE,
+                             countsFromAbundance="lengthScaledTPM")
 
   # these should not be exactly the same
   # lengthScaledTPM is very close, but adjusted for bias
-  expect_true(!all(txi$counts[,1] == txi.cfa$counts[,1]))
-  expect_true(!all(txi$counts[,1] == txi.cfa.len$counts[,1]))
+  expect_true(!all(txi$counts[,1] == txi.S$counts[,1]))
+  expect_true(!all(txi$counts[,1] == txi.LS$counts[,1]))
 
+  # what if someone sumToGene() with CFA="no" after it was non-no
+  expect_warning({
+    txi.sum.S <- summarizeToGene(txi.tx.S, tx2gene=tx2gene,
+                                 countsFromAbundance="no")
+  }, "incoming counts")
+  expect_true(txi.sum.S$countsFromAbundance == "scaledTPM")
+
+  expect_warning({
+    txi.sum.LS <- summarizeToGene(txi.tx.LS, tx2gene=tx2gene,
+                                  countsFromAbundance="no")
+  }, "incoming counts")
+  expect_true(txi.sum.LS$countsFromAbundance == "lengthScaledTPM")
+  
 })
