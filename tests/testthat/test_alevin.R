c94ec700bb1d5e8cfa9803f87ee1ecf7a87e9da1
mikelove <michaelisaiahlove@gmail.com>
2019-06-21 08:16:40 -0400

    small fix to test

--- tests/testthat/test_alevin.R
+++ tests/testthat/test_alevin.R
@@ -15,6 +15,6 @@ test_that("import alevin works", {
   matrix.file <- file.path(dir,"alevin/neurons_900_v014/alevin/quants_mat.mtx.gz")
   mat <- Matrix::readMM(matrix.file)
   mat <- t(as.matrix(mat))
-  expect_true(max(mat - unname(txi$counts)) < 1e-6)
+  expect_true(max(abs(mat - unname(txi$counts))) < 1e-6)
   
 })
